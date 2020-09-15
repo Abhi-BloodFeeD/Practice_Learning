8cdb8fe4d0e43e730137e8ed326b379c0ea6d603
bloodfeed <abhinavmetta@gmail.com>
2020-09-15 17:11:02 +0530

    MergeSort Added //REFER FOR LEARNING//

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,51 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define gc getchar_unlocked
-#define fo(i,n) for(ll i=0;i<n;i++)
-#define Fo(i,k,n) for(ll i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
-#define ll long long
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define itr(it, a) for(auto it = a.begin(); it != a.end(); it++)
-#define PI 3.1415926535897932384626
-#define w(t) ll t;cin>>t;while(t--)
-typedef pair<int, int>	pii;
-typedef pair<ll, ll>	pl;
-typedef vector<int>		vi;
-typedef vector<ll>		vl;
-typedef vector<pii>		vpii;
-typedef vector<pl>		vpl;
-typedef vector<vi>		vvi;
-typedef vector<vl>		vvl;
-mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-    uniform_int_distribution<int> uid(0,lim-1);
-    return uid(rang);
+
+template<typename Comparable>
+void mergeSort(vector<Comparable> &a)
+{
+    vector<Comparable> tmpArray(a.size());
+    mergeSort(a,tmpArray,0,a.size()-1);
+}
+template<typename Comparable>
+void mergeSort(vector<Comparable>&a,vector<Comparable>&tmpArray,int left,int right)
+{
+    if(left<right)
+    {
+        int center = (left+right)/2;
+        mergeSort(a,tmpArray,left,center);
+        mergeSort(a,tmpArray,center+1,right);
+        merge(a,tmpArray,left,center+1,right);
+    }
+
+}
+template<typename Comparable>
+void merge(vector<Comparable>&a,vector<Comparable>&tmpArray,int leftPos,int rightPos,int rightEnd)
+{
+    int leftEnd = rightPos-1;
+    int tmpPos = leftPos;
+    int numElements =rightEnd-leftPos+1;
+    //Main Loop
+
+    while(leftPos<=leftEnd && rightPos<=rightEnd)
+        if(a[leftPos]<=a[rightPos])
+            tmpArray[tmpPos++]=std::move(a[leftPos+1]);
+        else 
+            tmpArray[tmpPos++]=std::move(a[rightPos++]);
+    while(leftPos<=leftEnd) //copy rest
+        tmpArray[tmpPos++]=std::move(a[leftPos++]);
+    while(rightPos<=rightEnd)
+        tmpArray[tmpPos++]=std::move(a[rightPos++]);
+    //Copy tmpArray back
+    for(int i=0;i<numElements;++i,--rightEnd)
+        a[rightEnd]=std::move(tmpArray[rightEnd]);
+        
 }
 
-const int mod = 1000000007;
-const int N = 3e5, M = N;
-//=======================
-
-int main() {
-    //srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    ////CODE HERE ----------/////
-    
-
-
+int main(){
     return 0;
 }
